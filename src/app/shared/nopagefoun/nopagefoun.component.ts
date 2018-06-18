7142fb7453757392bfb5dbedf925a35d767eefe7
UrbanRude <urban.rude@gmail.com>
2018-06-17 19:27:22 -0500

    Frist commit

---
+++ src/app/shared/nopagefoun/nopagefoun.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefoun',
+  templateUrl: './nopagefoun.component.html',
+  styles: []
+})
+export class NopagefounComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
